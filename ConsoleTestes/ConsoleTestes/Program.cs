2bf9435821e962367d30cd8ebf32ecc871af5b01
maanoel <manoelvitorbrito@gmail.com>
2022-04-12 02:04:09 -0300

    medidor de contagem teste

---
+++ ConsoleTestes/ConsoleTestes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleTestes
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var linhas = File.ReadLines($@"C:\Users\manoel.vitor\Desktop\teste\C-Testes\Numeros.txt");
+      int linhaAnterior = 0;
+      int contadorMedidorMaiorQueAnterior = -1;
+
+      foreach(var linha in linhas)
+      {
+        if(int.Parse(linha) > linhaAnterior) {
+          contadorMedidorMaiorQueAnterior++;
+        }
+
+        linhaAnterior = int.Parse(linha);
+      }
+
+      Console.Write($@"O medidor ficou maior que a contagem anterior: { contadorMedidorMaiorQueAnterior} vezes." );
+    }
+  }
+}
