ffc935aeb0aeb349bbbcd022cc5aea38aadfc194
maanoel <manoelvitorbrito@gmail.com>
2022-04-12 21:36:04 -0300

    criando comando com argumentos que vem a travÚs de um arquivo

---
+++ ConsoleTestes/ConsoleTestes/CommandoCreator.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleTestes
+{
+  public class CommandoCreator
+  {
+    public Submarino Submarino { get; }
+    public string NomeComando { get; }
+    public int Argumento { get; }
+
+    public CommandoCreator(Submarino submarino, string nomeComando, int argumento)
+    {
+      Submarino = submarino;
+      NomeComando = nomeComando;
+      Argumento = argumento;
+    }
+
+    public IComando Criar() {
+
+      switch(NomeComando.Trim())
+      {
+        case "forward":
+          return new FowardCommand(Submarino, Argumento);
+        case "up":
+          return new UpCommand(Submarino, Argumento);
+        case "down":
+          return new DownCommand(Submarino, Argumento);
+      }
+
+      return null;
+    }
+  }
+}
--- ConsoleTestes/Tests/UnitTest1.cs
+++ ConsoleTestes/Tests/UnitTest1.cs
@@ -1,10 +1,11 @@
 using ConsoleTestes;
 using System;
+using System.IO;
 using Xunit;
 
 namespace Tests
 {
-  public class UnitTest1
+  public class UnitTest1 
   {
     [Fact]
     public void DeveMultiPlicarHorizontalProfundidadeERetornar150() {
@@ -25,6 +26,28 @@ namespace Tests
       int multiplicador = submarino.Multiplicar();
 
       Assert.Equal(150, multiplicador);
+    }
+
+    [Fact]
+    public void DeveLerOsComandoDeUmArquivoExteno()
+    {
+      ControleSubmarinoBuilder builder = new ControleSubmarinoBuilder();
+      Submarino submarino = new Submarino();
+
+      var linhas = File.ReadAllLines($@"C:\Users\manoel.vitor\Desktop\teste\C-Testes\Submarino.txt");
+
+      foreach(var linha in linhas)
+      {
+        string nomeComando = linha.Substring(0, linha.Length - 1);
+        int argumentoComando = int.Parse(linha.Substring(linha.Length - 1, 1));
+        IComando comando = new CommandoCreator(submarino, nomeComando, argumentoComando).Criar();
+
+        builder.AdicionarComando(comando);
+      }
+
+      builder.ExecutarComandos();
+
+      Assert.NotNull(builder);
     }
   }
 }
