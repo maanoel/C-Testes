bd17d5af3aee7c856c38a802c2fad5ebcca1a053
maanoel <manoelvitorbrito@gmail.com>
2022-04-14 02:32:34 -0300

    Convert.ToInt32 possui um mÚtodo que permite converter uma string em binßrio para um inteiro

--- ConsoleTestes/Tests/CalcularDoraGama.cs
+++ ConsoleTestes/Tests/CalcularDoraGama.cs
@@ -1,17 +1,47 @@
-﻿namespace Tests
+﻿using System;
+using System.Text;
+
+namespace Tests
 {
   internal class CalcularDoraGama : ICalculadoraSubmarino
   {
     private string[] linhas;
+    private string energiConsumida;
+    private int quantidadeBitsLinha = 0;
+
+    const int ligado = 1;
+    const int desligado = 0; 
 
     public CalcularDoraGama(string[] linhas)
     {
       this.linhas = linhas;
+      this.quantidadeBitsLinha = linhas[0].Length;
     }
 
     public int Calcular()
     {
-      return 22;
+      bool bitMaiorFrequencia = false;
+
+      for(int indiceCaracterer = 0; indiceCaracterer < quantidadeBitsLinha; indiceCaracterer++)
+      {
+        var contadorLigado = 0;
+        var contadorDesligado = 0;
+
+        for(int linha = 0; linha < linhas.Length; linha++)
+        {
+          var caracteresLinha = linhas[linha].ToCharArray();
+
+          if(int.Parse(caracteresLinha[indiceCaracterer].ToString()) == ligado) 
+            contadorLigado ++;
+          else
+            contadorDesligado ++;
+        }
+
+        bitMaiorFrequencia = contadorLigado > contadorDesligado;
+        energiConsumida += bitMaiorFrequencia ? ligado.ToString() :  desligado.ToString();
+      }
+
+      return Convert.ToInt32(energiConsumida, 2);
     }
   }
 }
