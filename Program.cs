6aad4813bf240390b447c11cd8960e6a7b477b03
Manoel Vitor Brito Moura <manoelvitorbrito@gmail.com>
2023-05-20 04:14:32 -0300

    feat: test book

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ sbyte n1 = 127;
 
 n1++;
 
+//_separate numeric
+int milion = 1_000_000_000;
+
+
 //Se uma variável passar os valores permitidos o retorno será o valor oposto do tipo de dados
 //No caso do sbyte seu valor oposto é -128, que será retornado sempre que passar do valor máximo
 // que é 127
